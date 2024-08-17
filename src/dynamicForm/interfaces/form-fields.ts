e687d86432913a1730e318f970cf8aa9af675515
RodrigoSHL <shl.catalan@gmail.com>
2024-08-17 12:34:20 -0400

    FEAT: add new type : file

--- src/dynamicForm/interfaces/form-fields.ts
+++ src/dynamicForm/interfaces/form-fields.ts
@@ -1,7 +1,7 @@
 export interface FormFields {
   name: string;
   label: string;
-  type: "text" | "email" | "boolean" | "text-area" | "array-text";
+  type: "text" | "email" | "boolean" | "text-area" | "array-text" | "file";
   visible: boolean;
   default?: any;
   defaultValue?: string;
